bacca4bf5eb748b22274742fe23a5034a7f5fd1e
Mike <schumami@hochschule-trier.de>
2018-03-01 10:58:07 +0100

    moved deleteAll function around

--- index.php
+++ index.php
@@ -7,6 +7,20 @@
     </head>
     <body class="center">
         
+        <?php
+            function deleteAll() {
+                echo "this is called";
+                $entryFile = fopen("resources/ListEntries.txt","r,w") or die("File not found?");
+                while(($line=fgets($entryFile)!=false)){
+                    if(explode(" ",$line)[1]==1){
+                        $entries = file_get_contents($entryFile);
+                        $entries = str_replace($line,'',$entries);
+                        file_put_contents($entryFile, $entries);
+                    }
+                }
+            }
+        ?>
+        
         <div class="center">
         <h1>Simple Todo list</h1>
         <table id="entries">
@@ -45,6 +59,8 @@
             <input type="submit" value="Add to list" onclick="addEntry();"/>
         </form>
         
-        <input id="deleteAll" type="button" value="Delete Completed" class="delete" onclick="deleteAll();"/>
+        <button id="deleteAll" class="delete" onclick="deleteAll();">Delete Completed</button>
+            
+        
     </body>
 </html>
